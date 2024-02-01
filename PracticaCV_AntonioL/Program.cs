7c71d0554aadf8524de189c9ddeb27a8e7d11bcb
antoniooo77 <antoniioo@outlokk.es>
2024-02-01 10:53:18 +0100

    Estructura del Programa de PracticaCV_AntonioL

--- PracticaCV_AntonioL/Program.cs
+++ PracticaCV_AntonioL/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // CONSTANTES 
+
+            // VARIABLES 
+
+            // iNICIALIZACION
+
+            // ENTRADA
+
+            // PROCESO
+
+            // SALIDA
         }
     }
 }
