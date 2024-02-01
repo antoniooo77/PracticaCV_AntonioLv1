435007d44d2bb058328976286a75f638c93cee18
antoniooo77 <antoniioo@outlokk.es>
2024-02-01 11:09:14 +0100

    Impresion de mi nombre AntonioL

--- PracticaCV_AntonioL/Program.cs
+++ PracticaCV_AntonioL/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace PracticaCV_AntonioL
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PracticaCV_AntonioL
 {
     internal class Program
     {
@@ -15,6 +17,7 @@
             // PROCESO
 
             // SALIDA
+            Console.WriteLine("Mi nombre es: AntonioL ");
         }
     }
 }
